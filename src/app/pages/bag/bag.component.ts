10d12cc6de846d2c746e2a4b334040535c1292b5
maxomoxo <marco.haider02@gmail.com>
2020-12-21 12:07:54 +0100

    stepper added

--- src/app/pages/bag/bag.component.ts
+++ src/app/pages/bag/bag.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Bag } from './bag';
 import { Filter, BagService } from './bag.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'ngx-bag',
@@ -13,15 +15,16 @@ export class BagComponent implements OnInit {
   
   items$: Observable<Bag[]>;
   itemsNumber$: Observable<number>;
+  firstForm: FormGroup;
+  secondForm: FormGroup;
+  thirdForm: FormGroup;
 
-  constructor(private bagService: BagService) {
+  constructor(private bagService: BagService, private fb: FormBuilder) {
     this.items$ = this.bagService.filteredItems();
     this.itemsNumber$ = this.bagService.filteredItemsNumber();
   }
 
-  ngOnInit(){
-    
-  }
+ 
 
   changeFilter(filterValue: Filter) {
     this.bagService.setFilter(filterValue);
@@ -43,6 +46,35 @@ export class BagComponent implements OnInit {
 
   onDelete(item) {
     this.bagService.delete(item);
+  }
+
+  
+  
+
+  ngOnInit() {
+    this.firstForm = this.fb.group({
+      firstCtrl: ['', Validators.required],
+    });
+
+    this.secondForm = this.fb.group({
+      secondCtrl: ['', Validators.required],
+    });
+
+    this.thirdForm = this.fb.group({
+      thirdCtrl: ['', Validators.required],
+    });
+  }
+
+  onFirstSubmit() {
+    this.firstForm.markAsDirty();
+  }
+
+  onSecondSubmit() {
+    this.secondForm.markAsDirty();
+  }
+
+  onThirdSubmit() {
+    this.thirdForm.markAsDirty();
   }
 
 }
--- src/app/pages/bag/bag.module.ts
+++ src/app/pages/bag/bag.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
-import { NbCardModule, NbAccordionModule, NbIconModule, NbButtonModule, NbInputModule, NbListModule, NbSelectModule, NbCheckboxModule} from '@nebular/theme';
+import { NbCardModule, NbAccordionModule, NbIconModule, NbButtonModule, NbInputModule, NbListModule, NbSelectModule, NbCheckboxModule, NbStepperModule} from '@nebular/theme';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { BagComponent } from './bag.component';
 import { BagItemComponent } from './bag-item/bag-item.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     NbCardModule,
+    FormsModule,
+    ReactiveFormsModule,
     ThemeModule,
     NbAccordionModule,
     NbIconModule,
@@ -15,6 +18,7 @@ import { BagItemComponent } from './bag-item/bag-item.component';
     NbInputModule,    
     NbCheckboxModule,
     NbListModule,
+    NbStepperModule,
     NbSelectModule
   ],
   declarations: [
