43275810ab6b12f6abf14aeb3226c7fc847bbde7
David Obermair <46714163+Obermair@users.noreply.github.com>
2021-01-12 14:32:49 +0100

    Update pages-menu.ts

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,13 +2,13 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'Home',
+    title: 'Produkte',
     icon: 'shopping-cart-outline',
     link: '/pages/dashboard',
     home: true,
   },
   {
-    title: 'Tasche',
+    title: 'Einkauf',
     icon: 'shopping-bag-outline',
     link: '/pages/bag',
     home: true,
